afa29e02a05582a774ab7b3090b17741a101a552
David <dducrest@gmail.com>
2014-03-14 13:47:42 -0400

    Removed access to create,edit,destroy game models
    Also added rails_12factor as heroku requested

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GameHost::Application.routes.draw do
-  resources :games
+  resources :games, :except => [:new, :edit, :delete]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
